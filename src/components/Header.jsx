2f928c3ffa3ec312152fdf44c64de76a5ee59834
virag-ky <vkormoczy@gmail.com>
2025-01-03 22:26:09 +0200

    Add search component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
+import { IoSearchSharp } from "react-icons/io5";
+
 function Header() {
   return (
     <header>
       <h1>A Collection of Front-End Cheat Sheets</h1>
+      <form>
+        <search>
+          <input
+            type="search"
+            name="search"
+            id="search"
+            placeholder="Search topics..."
+          />
+        </search>
+        <IoSearchSharp className="search-icon" />
+      </form>
     </header>
   );
 }

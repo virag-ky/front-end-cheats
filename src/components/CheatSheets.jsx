a98a1e58c2c26667f1f20bde3d712f240a3529bd
virag-ky <vkormoczy@gmail.com>
2025-01-05 16:44:15 +0200

    Install react-code-block package

--- src/components/CheatSheets.jsx
+++ src/components/CheatSheets.jsx
@@ -1,5 +1,44 @@
+import { CopyBlock, shadesOfPurple } from "react-code-blocks";
+
 function CheatSheets() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="cheatsheet-container">
+        <div className="cheatsheet">
+          <CopyBlock
+            className="code"
+            text="&lt;h1&gt;Heading 1&lt;/h1&gt;
+&lt;p&gt;Paragraph text here.&lt;/p&gt;
+&lt;a href='url'&gt;Link text&lt;/a&gt;
+&lt;img src='image.jpg' alt='description'&gt;
+&lt;ul&gt;
+    &lt;li&gt;List Item&lt;/li&gt;
+&lt;/ul&gt;
+&lt;div&gt;A block-level element&lt;/div&gt;"
+            language="html"
+            theme={shadesOfPurple}
+            codeBlock
+          />
+        </div>
+        <div className="cheatsheet">
+          <CopyBlock
+            className="code"
+            text="&lt;h1&gt;Heading 1&lt;/h1&gt;
+&lt;p&gt;Paragraph text here.&lt;/p&gt;
+&lt;a href='url'&gt;Link text&lt;/a&gt;
+&lt;img src='image.jpg' alt='description'&gt;
+&lt;ul&gt;
+    &lt;li&gt;List Item&lt;/li&gt;
+&lt;/ul&gt;
+&lt;div&gt;A block-level element&lt;/div&gt;"
+            language="html"
+            theme={shadesOfPurple}
+            codeBlock
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default CheatSheets;

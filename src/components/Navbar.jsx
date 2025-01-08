b2b753e6534e58e8b831a3c236c9fd25fce78b21
virag-ky <vkormoczy@gmail.com>
2025-01-08 11:48:10 +0200

    Add scroll to top functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 function Navbar() {
+  function handleScrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <nav>
-      <div className="app-name-container">
+      <div className="app-name-container" onClick={handleScrollToTop}>
         <img src="/icon.svg" alt="app logo" />
         <p className="app-name">FrontEndCheats</p>
       </div>

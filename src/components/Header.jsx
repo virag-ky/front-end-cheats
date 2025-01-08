5e8513fb63a80b7b2b00929152671ca324302d81
virag-ky <vkormoczy@gmail.com>
2025-01-08 12:02:54 +0200

    Add language buttons and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,8 @@
 import { IoSearchSharp } from "react-icons/io5";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { FaJsSquare } from "react-icons/fa";
+import { FaReact } from "react-icons/fa";
 
 function Header() {
   return (
@@ -15,6 +19,20 @@ function Header() {
         </search>
         <IoSearchSharp className="search-icon" />
       </form>
+      <div className="languages-container">
+        <button>
+          <FaHtml5 size={25} /> HTML
+        </button>
+        <button>
+          <FaCss3Alt size={25} /> CSS
+        </button>
+        <button>
+          <FaJsSquare size={25} /> JavaScript
+        </button>
+        <button>
+          <FaReact size={25} /> React
+        </button>
+      </div>
     </header>
   );
 }

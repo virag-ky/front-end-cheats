74b5cc31d909b14277b4aebff1c54ebfff2074b6
virag-ky <vkormoczy@gmail.com>
2025-01-19 16:51:19 +0200

    Add styles to indieboosting section

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@indieboosting/react": "@indieboosting/react/dist/main.js",
+    },
+  },
 });
